1927c09e85a52a9c2360b77f5d57214f67f62ab3
Mila Hose <mila.hose@gmail.com>
2020-09-05 22:50:39 -0700

    Add init files

---
+++ CSCI431ProjectFour/MilaHosePyramid.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var canvas;
+var gl;
+
+var NumVertices = 36;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [0, 0, 0];
+
+var thetaLoc;
+
+window.onload = function init() {
+  canvas = document.getElementById('gl-canvas');
+
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) alert('WebGL is not available');
+
+  colorCube();
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+  gl.enable(gl.DEPTH_TEST);
+
+  //  Load shaders and initialize attribute buffers
+  const program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+  gl.useProgram(program);
+
+  const cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+  const vColor = gl.getAttribLocation(program, 'vColor');
+  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vColor);
+
+  const vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+
+  const vPosition = gl.getAttribLocation(program, 'vPosition');
+  gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  thetaLoc = gl.getUniformLocation(program, 'theta');
+
+  // event listeners for buttons
+  document.getElementById('xButton').onclick = () => (axis = xAxis);
+  document.getElementById('yButton').onclick = () => (axis = yAxis);
+  document.getElementById('zButton').onclick = () => (axis = zAxis);
+
+  render();
+}
+
+function colorCube() {
+  quad(1, 0, 3, 2);
+  quad(2, 3, 7, 6);
+  quad(3, 0, 4, 7);
+  quad(6, 5, 1, 2);
+  quad(4, 5, 6, 7);
+  quad(5, 4, 0, 1);
+}
+
+function quad(a, b, c, d) {
+  const vertices = [
+    vec4( -0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5, -0.5, -0.5, 1.0 ),
+    vec4( -0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5, -0.5, -0.5, 1.0 )
+  ];
+
+  const vertexColors = [
+    [0.0, 0.0, 0.0, 1.0],  // black
+    [1.0, 0.0, 0.0, 1.0],  // red
+    [1.0, 1.0, 0.0, 1.0],  // yellow
+    [0.0, 1.0, 0.0, 1.0],  // green
+    [0.0, 0.0, 1.0, 1.0],  // blue
+    [1.0, 0.0, 1.0, 1.0],  // magenta
+    [0.0, 1.0, 1.0, 1.0],  // cyan
+    [1.0, 1.0, 1.0, 1.0]   // white
+  ];
+
+  // We need to parition the quad into two triangles in order for
+  // WebGL to be able to render it.  In this case, we create two
+  // triangles from the quad indices
+
+  //vertex color assigned by the index of the vertex
+  const indices = [ a, b, c, a, c, d ];
+  for (let i = 0; i < indices.length; ++i) {
+    points.push( vertices[indices[i]] );
+    //colors.push( vertexColors[indices[i]] );
+
+    // for solid colored faces use
+    colors.push(vertexColors[a]);
+  }
+}
+
+function render() {
+  gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  theta[axis] += 2.0;
+  gl.uniform3fv(thetaLoc, theta);
+  gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
+  requestAnimFrame(render);
+}
