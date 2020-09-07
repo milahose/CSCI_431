9beb326b064ad932f41906be37a8dcc53a0daae4
Mila Hose <mila.hose@gmail.com>
2020-09-07 14:17:45 -0700

    Fix extra space

--- CSCI431ProjectFive/MilaHosePyramid.js
+++ CSCI431ProjectFive/MilaHosePyramid.js
@@ -1,161 +1,139 @@
-"use strict";
+'use strict';
 
-var canvas;
-var gl;
+let canvas;
+let gl;
 
-var NumVertices  = 36;
+let eye;
+let modelViewMatrix;
+let projectionMatrix;
+let modelViewMatrixLoc;
+let projectionMatrixLoc;
 
-var pointsArray = [];
-var colorsArray = [];
+let axis = 0;
+let xAxis = 0;
+let yAxis = 1;
+let zAxis = 2;
 
-var vertices = [
-    vec4(-0.5, -0.5,  1.5, 1.0),
-    vec4(-0.5,  0.5,  1.5, 1.0),
-    vec4(0.5,  0.5,  1.5, 1.0),
-    vec4(0.5, -0.5,  1.5, 1.0),
-    vec4(-0.5, -0.5, 0.5, 1.0),
-    vec4(-0.5,  0.5, 0.5, 1.0),
-    vec4(0.5,  0.5, 0.5, 1.0),
-    vec4( 0.5, -0.5, 0.5, 1.0)
-];
+let thetav = [0, 0, 0]; // 3 angles for rotate around x, y, and z axises.
+let thetavLoc; // use to bind with thetav in vertex shader
+let flag = false; // to indicate if the pyramid needs to rotate
 
-var vertexColors = [
-    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan
-    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
-];
+let near = 0.3;
+let far = 3.0;
+let radius = 4.0;
+let theta = 0.0;
+let phi = 0.0;
+let dr = 5.0 * Math.PI / 180.0;
 
+let fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
+let aspect = 1.0; // Viewport aspect ratio
 
-var near = 0.3;
-var far = 3.0;
-var radius = 4.0;
-var theta  = 0.0;
-var phi    = 0.0;
-var dr = 5.0 * Math.PI/180.0;
+let pointsArray = [];
+let colorsArray = [];
 
-var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
-var  aspect = 1.0;       // Viewport aspect ratio
-
-var modelViewMatrix, projectionMatrix;
-var modelViewMatrixLoc, projectionMatrixLoc;
-var eye;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
+const vertices = [
+	vec4(0.5, -0.2722,  1.2886, 1.0),
+	vec4(0.0,  -0.2772,  1-0.5773, 1.0),
+	vec4(-0.5,  -0.2722,  1.2886, 1.0),
+	vec4(0.0, 0.5443,  1.0, 1.0)
+];
 
+const vertexColors = [
+	vec4(0.0, 0.0, 0.0, 1.0), // black
+	vec4(1.0, 0.0, 0.0, 1.0), // red
+	vec4(0.0, 1.0, 0.0, 1.0), // green
+	vec4(0.0, 0.0, 1.0, 1.0)  // blue
+];
 
-function quad(a, b, c, d) {
-     pointsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
-     pointsArray.push(vertices[b]);
-     colorsArray.push(vertexColors[a]);
-     pointsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
-     pointsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
-     pointsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
-     pointsArray.push(vertices[d]);
-     colorsArray.push(vertexColors[a]);
+const triple = (a, b, c) => {
+	pointsArray.push(vertices[a]); 
+	colorsArray.push(vertexColors[a]); 
+	pointsArray.push(vertices[b]); 
+	colorsArray.push(vertexColors[a]); 
+	pointsArray.push(vertices[c]); 
+	colorsArray.push(vertexColors[a]);
 }
 
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+const colorPyramid = () => {
+	triple(0, 2, 1);
+	triple(1, 2, 3);
+	triple(2, 3, 0);
+	triple(3, 0, 1);
 }
-
 
 window.onload = function init() {
-    canvas = document.getElementById( "gl-canvas" );
+	canvas = document.getElementById('gl-canvas');
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+	gl = WebGLUtils.setupWebGL( canvas );
+	if (!gl) alert('WebGL is nott available');
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	aspect = canvas.width / canvas.height;
+	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	gl.enable(gl.DEPTH_TEST);
 
-    aspect =  canvas.width/canvas.height;
+	//  Load shaders and initialize attribute buffers
+	const program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+	gl.useProgram(program);
 
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+	colorPyramid();
 
-    gl.enable(gl.DEPTH_TEST);
+	const cBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+	const vColor = gl.getAttribLocation(program, 'vColor');
+	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vColor);
 
-    colorCube();
+	const vBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+	const vPosition = gl.getAttribLocation(program, 'vPosition');
+	gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vPosition);
 
-    var vColor = gl.getAttribLocation( program, "vColor" );
-    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vColor);
+	thetavLoc = gl.getUniformLocation(program, 'thetav');
+	modelViewMatrixLoc = gl.getUniformLocation(program, 'modelViewMatrix');
+	projectionMatrixLoc = gl.getUniformLocation(program, 'projectionMatrix');
 
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+	// sliders & buttons for viewing parameters
+  document.getElementById('zFarSlider').onchange = ({ target }) => (far = target.value);
+  document.getElementById('zNearSlider').onchange = ({ target }) => (near = target.value);
+  document.getElementById('radiusSlider').onchange = ({ target }) => (radius = target.value);
+  document.getElementById('thetaSlider').onchange = ({ target }) => (theta = target.value * Math.PI / 180.0);
+  document.getElementById('phiSlider').onchange = ({ target }) => (phi = target.value * Math.PI / 180.0);
+  document.getElementById('aspectSlider').onchange = ({ target }) => (aspect = target.value);
+  document.getElementById('fovSlider').onchange = ({ target }) => (fovy = target.value);
+	document.getElementById('xButton').onclick = () => (axis = xAxis);
+	document.getElementById('yButton').onclick = () => (axis = yAxis);
+	document.getElementById('zButton').onclick = () => (axis = zAxis);
+	document.getElementById('ButtonT').onclick = () => (flag = !flag);
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-
-// sliders for viewing parameters
-
-    document.getElementById("zFarSlider").onchange = function(event) {
-        far = event.target.value;
-    };
-    document.getElementById("zNearSlider").onchange = function(event) {
-        near = event.target.value;
-    };
-    document.getElementById("radiusSlider").onchange = function(event) {
-       radius = event.target.value;
-    };
-    document.getElementById("thetaSlider").onchange = function(event) {
-        theta = event.target.value* Math.PI/180.0;
-    };
-    document.getElementById("phiSlider").onchange = function(event) {
-        phi = event.target.value* Math.PI/180.0;
-    };
-    document.getElementById("aspectSlider").onchange = function(event) {
-        aspect = event.target.value;
-    };
-    document.getElementById("fovSlider").onchange = function(event) {
-        fovy = event.target.value;
-    };
-
-    render();
+  render();
 }
 
+const render = function() {
+	gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-var render = function(){
+	eye = vec3(
+		radius * Math.sin(theta) * Math.cos(phi),
+		radius * Math.sin(theta) * Math.sin(phi), 
+		radius * Math.cos(theta)
+	);
 
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	if (flag) thetav[axis] += 2.0;
+	modelViewMatrix = lookAt(eye, at , up);
+	projectionMatrix = perspective(fovy, aspect, near, far);
 
-    eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
-        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
-    modelViewMatrix = lookAt(eye, at , up);
-    projectionMatrix = perspective(fovy, aspect, near, far);
+	gl.uniform3fv(thetavLoc, thetav);
+	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+	gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
 
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
-
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
-    requestAnimFrame(render);
+	gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
+	requestAnimFrame(render);
 }
