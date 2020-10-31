cb0a9ba30abc81a9daa19781155d600f11ef9672
Mila Hose <mila.hose@gmail.com>
2020-10-31 12:41:50 -0700

    More updates

--- ProjectEight/main.js
+++ ProjectEight/main.js
@@ -5,12 +5,6 @@ let canvas;
 var get_pixel_flag = false;
 var colour_x;
 var colour_y;
-var vbo;
-var vbo_hue;
-var vbo_chosen;
-var sp;
-var sp_hue;
-var sp_chosen;
 var chosen_fc_loc;
 var box_g_loc;
 var hue_g_loc;
@@ -20,8 +14,8 @@ var hue_deg = 0.0;
 var sat_pc = 0.0;
 var val_pc = 0.0;
 var gamma = 1.0;
-let program, pb;
-let vBuffer, pbBuffer;
+let pa, pb, pc;
+let paBuffer, pbBuffer, pcBuffer;
 
 var data = [
 	-1.0,  1.0,
@@ -69,112 +63,21 @@ window.onload = () => {
 
 	gl.enable(gl.DEPTH_TEST);
 
-	program = initShaders(gl, 'vertex-shader-a', 'fragment-shader-a');
-	gl.useProgram(program);
+	// ======= Part A ======= // 
+	pa = initShaders(gl, 'vertex-shader-a', 'fragment-shader-a');
+	gl.useProgram(pa);
 	
-	vBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+	paBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, paBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(data), gl.STATIC_DRAW);
-	
-	// vbo = gl.createBuffer ();
-	// gl.bindBuffer (gl.ARRAY_BUFFER, vbo);
-	// gl.bufferData (gl.ARRAY_BUFFER, new Float32Array (data), gl.STATIC_DRAW);
-	
-	// set up shader
-	// var vs_src = (
-	//  `attribute vec2 p;
-	// 	varying vec2 vs;
-	// 	void main () {
-	// 		vs.x = 0.5 * (p.x + 1.0);
-	// 		vs.y = 0.5 * (p.y + 1.0);
-	// 		gl_Position = vec4 (p.x * 0.75 - 0.25, p.y * 0.75 + 0.25, 0.0, 1.0);
-	// 	}`);
-	// var vs_src = [
-	// 	"attribute vec2 p;\n",
-	// 	"varying vec2 vs;\n",
-	// 	"void main () {\n",
-	// 	"  vs.x = 0.5 * (p.x + 1.0);\n",
-	// 	"  vs.y = 0.5 * (p.y + 1.0);\n",
-	// 	"  gl_Position = vec4 (p.x * 0.75 - 0.25, p.y * 0.75 + 0.25, 0.0, 1.0);\n",
-	// 	"}"
-	// ].join('\n');
-	// var fs_src = [
-	// 	"precision mediump float;\n",
-	// 	"uniform float h;\n",
-	// 	"uniform float g;\n",
-	// 	"varying vec2 vs;\n",
-	// 	"void main () {\n",
 
-	// 	"float chroma = vs.x * vs.y;\n",
-	// 	"float h_dash = h / 60.0;\n",
-	// 	"float x = chroma * (1.0 - abs (mod (h_dash, 2.0) - 1.0));\n",
-	// 	"vec3 rgb = vec3 (0.0, 0.0, 0.0);\n",
-	// 	"if (h_dash < 1.0) {\n",
-	// 	"	rgb.r = chroma;\n",
-	// 	"	rgb.g = x;\n",
-	// 	"} else if (h_dash < 2.0) {\n",
-	// 	"	rgb.r = x;\n",
-	// 	"	rgb.g = chroma;\n",
-	// 	"} else if (h_dash < 3.0) {\n",
-	// 	"	rgb.g = chroma;\n",
-	// 	"	rgb.b = x;\n",
-	// 	"} else if (h_dash < 4.0) {\n",
-	// 	"	rgb.g = x;\n",
-	// 	"	rgb.b = chroma;\n",
-	// 	"} else if (h_dash < 5.0) {\n",
-	// 	"	rgb.r = x;\n",
-	// 	"	rgb.b = chroma;\n",
-	// 	"} else {\n",
-	// 	"	rgb.r = chroma;\n",
-	// 	"	rgb.b = x;\n",
-	// 	"}\n",
-	// 	"float minv = vs.x - chroma;\n",
-	// 	"rgb += minv;\n",
-	// 	"  vec3 gamma = vec3 (g, g, g);\n",
-	// 	"  gl_FragColor = vec4 (pow (rgb, 1.0 / gamma), 1.0);\n",
-	// 	"}"
-	// ].join('\n');
-	// console.log(fs_src)
-	// var vs = gl.createShader (gl.VERTEX_SHADER);
-	// gl.shaderSource (vs, vs_src);
-	// gl.compileShader (vs);
-	
-	// var fs = gl.createShader (gl.FRAGMENT_SHADER);
-	// gl.shaderSource (fs, fs_src);
-	// gl.compileShader (fs);
-	
-	// sp = gl.createProgram ();
-	// gl.attachShader (sp, vs);
-	// gl.attachShader (sp, fs);
-	// gl.bindAttribLocation (sp, 0, "p");
-	// gl.linkProgram (sp);
-
-	// gl.validateProgram (sp);
-
-	h_loc = gl.getUniformLocation (program, "h");
-	box_g_loc = gl.getUniformLocation (program, "g");
-	gl.useProgram (program);
+	h_loc = gl.getUniformLocation (pa, "h");
+	box_g_loc = gl.getUniformLocation (pa, "g");
+	gl.useProgram (pa);
 	gl.uniform1f (h_loc, 0.0);
 	gl.uniform1f (box_g_loc, 1.0);
-	
-	// set up hue chooser
-	// var data_hue = [
-	// 	1.0,  1.0, 1.0, 0.0, 0.0,
-	// 	-1.0,  1.0, 1.0, 0.0, 0.0,
-	// 	1.0,  0.667, 1.0, 0.0, 1.0,
-	// 	-1.0,  0.667, 1.0, 0.0, 1.0,
-	// 	1.0,  0.333, 0.0, 0.0, 1.0,
-	// 	-1.0,  0.333, 0.0, 0.0, 1.0,
-	// 	1.0,  0.0, 0.0, 1.0, 1.0,
-	// 	-1.0,  0.0, 0.0, 1.0, 1.0,
-	// 	1.0, -0.333, 0.0, 1.0, 0.0,
-	// 	-1.0, -0.333, 0.0, 1.0, 0.0,
-	// 	1.0, -0.667, 1.0, 1.0, 0.0,
-	// 	-1.0, -0.667, 1.0, 1.0, 0.0,
-	// 	1.0, -1.0, 1.0, 0.0, 0.0,
-	// 	-1.0, -1.0, 1.0, 0.0, 0.0
-		
-	// ];
+
+	// ======= Part B ======= // 
 	pb = initShaders(gl, 'vertex-shader-b', 'fragment-shader-b');
 	gl.useProgram(pb);
 	
@@ -182,107 +85,25 @@ window.onload = () => {
   gl.bindBuffer(gl.ARRAY_BUFFER, pbBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(data_hue), gl.STATIC_DRAW);
 
-	// vbo_hue = gl.createBuffer ();
-	// gl.bindBuffer (gl.ARRAY_BUFFER, vbo_hue);
-	// gl.bufferData (gl.ARRAY_BUFFER, new Float32Array (data_hue), gl.STATIC_DRAW);
-	
-	// set up shader
-	// let vs_src = [
-	// 	"attribute vec2 p;\n",
-	// 	"attribute vec3 c;\n",
-	// 	"varying vec3 fc;\n",
-	// 	"void main () {\n",
-	// 	"  fc = c;\n",
-	// 	"  gl_Position = vec4 (p.x * 0.125 + 0.875, p.y * 0.75 + 0.25, 0.0, 1.0);\n",
-	// 	"}"
-	// ].join('\n');
-	// let fs_src = [
-	// 	"precision mediump float;\n",
-	// 	"varying vec3 fc;\n",
-	// 	"uniform float g;\n",
-	// 	"void main () {\n",
-	// 	"  vec3 gamma = vec3 (g, g, g);\n",
-	// 	"  gl_FragColor = vec4 (pow (fc, 1.0 / gamma), 1.0);\n",
-	// 	"}"
-	// ].join('\n');
-	
-	// let vs = gl.createShader (gl.VERTEX_SHADER);
-	// gl.shaderSource (vs, vs_src);
-	// gl.compileShader (vs);
-	
-	// let fs = gl.createShader (gl.FRAGMENT_SHADER);
-	// gl.shaderSource (fs, fs_src);
-	// gl.compileShader (fs);
-	
-	// sp_hue = gl.createProgram ();
-	// gl.attachShader (sp_hue, vs);
-	// gl.attachShader (sp_hue, fs);
-	// gl.bindAttribLocation (sp_hue, 0, "p");
-	// gl.bindAttribLocation (sp_hue, 1, "c");
-	// gl.linkProgram (sp_hue);
-
-	// gl.validateProgram (sp_hue);
-
 	hue_g_loc = gl.getUniformLocation (pb, "g");
 	gl.useProgram (pb);
 	gl.uniform1f (hue_g_loc, 1.0);
 	
-	var data_chosen = [
-		-1.0,  1.0,
-		-1.0, -1.0,
-		 1.0,  1.0,
-		 1.0,  1.0,
-		-1.0, -1.0,
-		 1.0, -1.0
-	];
-	
-	// chosen square
-	vbo_chosen = gl.createBuffer ();
-	gl.bindBuffer (gl.ARRAY_BUFFER, vbo_chosen);
-	gl.bufferData (
-		gl.ARRAY_BUFFER, new Float32Array (data_chosen), gl.STATIC_DRAW);
-		
-	// set up shader
-	let vs_src = [
-		"attribute vec2 p;\n",
-		"void main () {\n",
-		"  gl_Position = vec4 (p.x, p.y * 0.2 - 0.75, 0.0, 1.0);\n",
-		"}"
-	].join('\n');
-	let fs_src = [
-		"precision mediump float;\n",
-		"uniform vec3 fc;\n",
-		"uniform float g;\n",
-		"void main () {\n",
-		"  vec3 gamma = vec3 (g, g, g);\n",
-		"  gl_FragColor = vec4 (pow (fc, 1.0 / gamma), 1.0);\n",
-		"}"
-	].join('\n');
-	
-	let vs = gl.createShader (gl.VERTEX_SHADER);
-	gl.shaderSource (vs, vs_src);
-	gl.compileShader (vs);
+	// ======= Part C ======= // 
+	pc = initShaders(gl, 'vertex-shader-c', 'fragment-shader-c');
+	gl.useProgram(pc);
 
-	
-	let fs = gl.createShader (gl.FRAGMENT_SHADER);
-	gl.shaderSource (fs, fs_src);
-	gl.compileShader (fs);
+	pcBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, pcBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(data_chosen), gl.STATIC_DRAW);
 
-	
-	sp_chosen = gl.createProgram ();
-	gl.attachShader (sp_chosen, vs);
-	gl.attachShader (sp_chosen, fs);
-	gl.bindAttribLocation (sp_chosen, 0, "p");
-	gl.linkProgram (sp_chosen);
-
-	gl.validateProgram (sp_chosen);
-
-	chosen_fc_loc = gl.getUniformLocation (sp_chosen, "fc");
-	chosen_g_loc = gl.getUniformLocation (sp_chosen, "g");
-	gl.useProgram (sp_chosen);
+	chosen_fc_loc = gl.getUniformLocation (pc, "fc");
+	chosen_g_loc = gl.getUniformLocation (pc, "g");
+	gl.useProgram (pc);
 	gl.uniform3f (chosen_fc_loc, 0.0, 0.0, 0.0);
 	gl.uniform1f (chosen_g_loc, 1.0);
 	
+	// ======= Click Event Stuff ======= // 
 	canvas.onmousedown = function (ev) {
 		get_pixel_flag = true;
 		// recursively get location within parent(s)
@@ -348,8 +169,8 @@ function main_loop () {
 	gl.cullFace (gl.BACK);
 	gl.frontFace (gl.CCW);
 
-	gl.useProgram (program);
-	gl.bindBuffer (gl.ARRAY_BUFFER, vBuffer);
+	gl.useProgram (pa);
+	gl.bindBuffer (gl.ARRAY_BUFFER, paBuffer);
 	gl.vertexAttribPointer (0, 2, gl.FLOAT, false, 8, 0);
 	gl.enableVertexAttribArray (0);
 	gl.drawArrays (gl.TRIANGLES, 0, 6);
@@ -365,8 +186,8 @@ function main_loop () {
 	gl.disableVertexAttribArray (0);
 	gl.disableVertexAttribArray (1);
 	
-	gl.useProgram (sp_chosen);
-	gl.bindBuffer (gl.ARRAY_BUFFER, vbo_chosen);
+	gl.useProgram (pc);
+	gl.bindBuffer (gl.ARRAY_BUFFER, pcBuffer);
 	gl.vertexAttribPointer (0, 2, gl.FLOAT, false, 8, 0);
 	gl.enableVertexAttribArray (0);
 	gl.drawArrays (gl.TRIANGLES, 0, 6);
@@ -394,7 +215,7 @@ function main_loop () {
 				hue_b = 0.0;
 			}
 			// hue colour affects main chooser box
-			gl.useProgram (program);
+			gl.useProgram (pa);
 			gl.uniform1f (h_loc, hue_deg);
 		}
 		
@@ -436,7 +257,7 @@ function main_loop () {
 		g += minv;
 		b += minv;
 		
-		gl.useProgram (sp_chosen);
+		gl.useProgram (pc);
 		gl.uniform3f (chosen_fc_loc, r, g, b);
 		
 
